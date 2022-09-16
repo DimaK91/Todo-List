75164e0091a1aaeb47929bcba7a6ef72ce66da72
Dima <dimontiksk@gmail.com>
2022-09-16 18:22:22 +0300

    add form

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,16 +1,18 @@
 import React from "react";
-
 import TodoListItem from "../todo-list-item/todo-list-item";
 import './todo-list.css';
 
-const TodoList = ({todos, onDeleted}) => {
+const TodoList = ({todos, onDeleted, onToggleimportant, onToggleDone}) => {
     const elements = todos.map((item) => {
         const {id, ...itemProps} = item;
         return (
             <li key={id} className='list-group-item'>
                 <TodoListItem
                     {...itemProps}
-                    onDeleted = {() => onDeleted(id)}/>
+                    onDeleted = {() => onDeleted(id)}
+                    onToggleImportant = {() => onToggleimportant(id)}
+                    onToggleDone = {() => onToggleDone(id)}
+                />
             </li>
         );
     });
